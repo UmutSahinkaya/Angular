39007c88123dded0467cff92e2c1c1c9cf68eb3b
Umut <umut.dev@example.com>
2026-02-15 11:09:55 +0300

    Angular Pipes

--- 8.Pipes/MyPipesExample/src/app/app.ts
+++ 8.Pipes/MyPipesExample/src/app/app.ts
@@ -1,20 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { TodoPipe } from './todo-pipe';
+import { TrCurrencyPipe } from 'tr-currency';
 
 @Component({
   selector: 'app-root',
-  imports: [FormsModule, TodoPipe],
+  imports: [CommonModule,TrCurrencyPipe],
   templateUrl: './app.html',
   styleUrls: ['./app.css'],
 })
 export class App {
- work:string="";
- todos:string[]=["domates","biber","soğan"];
- search:string="";
-
-  Save(){
-  this.todos.push(this.work);
-  this.work="";
-  }
+  name:string = "Angular Pipe Example";
+  date:Date = new Date();
+  num:number = 17002.25;
 }
