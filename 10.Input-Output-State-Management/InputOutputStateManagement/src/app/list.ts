aaa2691f86c45328f3dfef49acb50fdd90fcfecc
Umut <umut.dev@example.com>
2026-02-16 10:03:22 +0300

    output management is done

--- 10.Input-Output-State-Management/InputOutputStateManagement/src/app/list.ts
+++ 10.Input-Output-State-Management/InputOutputStateManagement/src/app/list.ts
@@ -1,30 +1,21 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-list',
-  imports: [],
+  imports: [FormsModule],
   template:`
     <h2>List</h2>
-    <table>
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>Name</th>
-          <th>Price</th>
-        </tr>
-      </thead>
-      <tbody>
-        @for(val of products,track val) {
-          <tr>
-            <td>{{$index}}</td> 
-            <td>{{val.name}}</td>
-            <td>{{val.price}}</td>
-          </tr>
-        }
-    </table>
+    <input [(ngModel)]="name"/>
+    <button (click)="save()">Save</button>
+    
   `
 })
 export class List {
-  @Input({required: true}) products: any[] = [];
-  @Input() title: string = '';
+  @Input() name:string = '';
+  @Output() myEvent= new EventEmitter<string>();
+
+  save(){
+    this.myEvent.emit(this.name);
+  }
 }
--- 10.Input-Output-State-Management/InputOutputStateManagement/src/app/products.ts
+++ 10.Input-Output-State-Management/InputOutputStateManagement/src/app/products.ts
@@ -6,15 +6,15 @@ import { List } from './list';
   imports: [List],
   template:`
     <h2>Products Component</h2><hr>
-    <app-list [products]="products" [title]="'Product List'"></app-list>
+    <p>{{name}}</p>
+    <app-list [name]="name" (myEvent)="save($event)"></app-list>
+    
   `
 })
 export class Products {
-  products:any[] = [
-    {name:'Bilgisayar',price:10000},
-    {name:'Telefon',price:5000},
-    {name:'Tablet',price:7000},
-    {name:'Monitör',price:3000},
-    {name:'Klavye',price:1000},
-  ]
+  name:string = 'Umut';
+
+  save(event: any) {
+    this.name = event;
+  }
 }
