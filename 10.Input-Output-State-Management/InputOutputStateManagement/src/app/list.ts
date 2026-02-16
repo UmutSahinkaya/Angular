256a858a5e8b7810c2dd1236af2e230f7fd81e20
Umut <umut.dev@example.com>
2026-02-16 09:36:44 +0300

    Proje olluşturuldu product ve list component oluşturuldu

---
+++ 10.Input-Output-State-Management/InputOutputStateManagement/src/app/list.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list',
+  imports: [],
+  template:`
+    <h2>List</h2>
+  `
+})
+export class List {
+
+}
---
+++ 10.Input-Output-State-Management/InputOutputStateManagement/src/app/products.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-products',
+  imports: [],
+  template:`
+    <h2>Products</h2>
+  `
+})
+export class Products {
+
+}
