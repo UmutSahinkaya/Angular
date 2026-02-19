5e1853e5721320511ad0dc7ec2c7493eeca36b04
Umut <umut.dev@example.com>
2026-02-19 10:50:46 +0300

    Implement Todo feature with async data fetching and error handling

--- 11.HttpClient/httpAsync/src/app/app.routes.ts
+++ 11.HttpClient/httpAsync/src/app/app.routes.ts
@@ -1,3 +1,24 @@
-import { Routes } from '@angular/router';
+import { ActivatedRouteSnapshot, Routes } from '@angular/router';
+import { Todo } from './todo';
+import { TodoDetail } from './todo-detail';
+import { Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component:Todo
+    },
+    {
+        path:':id',
+        component:TodoDetail,
+        resolve: {
+            todo:async ({params}:ActivatedRouteSnapshot)=>{
+                const http = Inject(HttpClient);
+                // await new Promise(resolve => setTimeout(resolve, 3000)); // Simulate a delay of 3 seconds
+                const id = params['id'];
+                return http.get(`https://jsonplaceholder.typicode.com/todos/`+id);
+            }
+        }
+    }
+];
