92acb4f34e302662805b5d857328863eebc1c8fb
Umut <umut.dev@example.com>
2026-02-19 09:22:52 +0300

    todo componenti ekledim gereksiz dosyaları çıkardım en sadece şekliyle başlıyorum

--- 11.HttpClient/httpAsync/src/app/app.ts
+++ 11.HttpClient/httpAsync/src/app/app.ts
@@ -1,12 +1,10 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  imports: [],
+  template:`
+    <h1>{{ title() }}</h1>`
 })
 export class App {
-  protected readonly title = signal('httpAsync');
 }
