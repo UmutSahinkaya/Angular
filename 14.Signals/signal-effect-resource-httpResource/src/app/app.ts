e5b31dc5cb414ef1313ada12fa266ab75f5bbf66
Umut <umut.dev@example.com>
2026-02-21 08:47:19 +0300

    resource kullanımından sonra

--- 14.Signals/signal-effect-resource-httpResource/src/app/app.ts
+++ 14.Signals/signal-effect-resource-httpResource/src/app/app.ts
@@ -1,31 +1,31 @@
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, computed, inject, resource, signal } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   imports: [],
   template: ``,
 })
-export class App implements OnInit {
-  todos = signal<any[]>([]);
+export class App {
+  readonly result = resource({
+    loader: async () => {
+      const res = await lastValueFrom(
+        this.#http.get<any[]>('https://jsonplaceholder.typicode.com/todos/'),
+      );
+      //const res = await fetch('https://jsonplaceholder.typicode.com/todos/').then(res=>res.json());
+
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+      return res;
+    },
+  });
+
+  readonly todos = computed(() => this.result.value() ?? []);
+  readonly loading = computed(() => this.result.isLoading() ?? false);
+  readonly error = computed(() => {
+    console.log(this.result.error());
+
+    return this.result.error() ? 'Something went wrong :(  Try again later.' : undefined;
+  });
   readonly #http = inject(HttpClient);
-  readonly loading= signal<boolean>(false);
-
-  ngOnInit() {
-    this.getTodos();
-  }
-
-  getTodos() {
-    this.loading.set(true);
-    this.#http.get<any[]>('https://jsonplaceholder.typicode.com/todos').subscribe({
-      next: (res) => {
-        this.todos.set(res);
-        this.loading.set(false);
-      },
-      error: (err: HttpErrorResponse) => {
-        console.log('Error fetching todos:', err);
-        this.loading.set(false);
-      },
-    });
-  }
 }
