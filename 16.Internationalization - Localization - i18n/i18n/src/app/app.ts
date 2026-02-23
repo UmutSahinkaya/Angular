6bf12316e9067176352279e7ec115dd99043b90d
Umut <umut.dev@example.com>
2026-02-23 08:57:28 +0300

    feat: update localization files and enhance language switching functionality

--- 16.Internationalization - Localization - i18n/i18n/src/app/app.ts
+++ 16.Internationalization - Localization - i18n/i18n/src/app/app.ts
@@ -1,11 +1,27 @@
-import { Component, signal } from '@angular/core';
-import { TranslocoModule } from '@jsverse/transloco';
+import { Component, inject, signal } from '@angular/core';
+import { TranslocoModule, TranslocoService } from '@jsverse/transloco';
 
 @Component({
   selector: 'app-root',
   imports: [TranslocoModule],
-  template:`
-  {{TITLE}}`
+  template: `
+    <ng-container *transloco="let t">
+      <h1>{{ t('TITLE') }}</h1>
+      <button (click)="changeLang('tr')">{{ t('LANGUAGE.TR') }}</button>
+      <button (click)="changeLang('en')">{{ t('LANGUAGE.EN') }}</button>
+    </ng-container>
+  `,
 })
 export class App {
+  readonly #transloco = inject(TranslocoService);
+
+  constructor() {
+    const lang = localStorage.getItem('lang') || 'en';
+    this.#transloco.setActiveLang(lang);
+  }
+
+  changeLang(lang: string) {
+    this.#transloco.setActiveLang(lang);
+    localStorage.setItem('lang', lang);
+  }
 }
